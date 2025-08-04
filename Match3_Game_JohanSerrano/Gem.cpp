eb5234d9666284015c7820f7e2afba910eefb232
JohanDanilo <johan.serrano.victor@est.una.ac.cr>
2025-08-03 21:03:26 -0600

    Rename main.cpp and remove unused code

--- Match3_Game_JohanSerrano/Gem.cpp
+++ Match3_Game_JohanSerrano/Gem.cpp
@@ -1,4 +1,6 @@
 #include "Gem.h"
+const int TILE_SIZE = 64;
+const int BOARD_SIZE = 8;
 
 Gem::Gem()
 {
@@ -10,13 +12,16 @@ Gem::Gem(int aType, const Texture& aTexture, const Vector2i aPosition)
 {
 	type = aType;
 	sprite.setTexture(aTexture);
+	sprite.setTextureRect(sf::IntRect(type * TILE_SIZE, 0, TILE_SIZE, TILE_SIZE));
 	boardPosition = aPosition;
+	sprite.setPosition(boardPosition.x * TILE_SIZE, boardPosition.y * TILE_SIZE);
 	setBoardPosition(aPosition); // esto asegura que la gema se posicione bien
 }
 
 void Gem::setType(int aType)
 {
 	type = aType;
+	sprite.setTextureRect(IntRect(type * TILE_SIZE, 0, TILE_SIZE, TILE_SIZE));
 }
 
 int Gem::getType()
---
+++ Match3_Game_JohanSerrano/main.cpp
@@ -0,0 +1,14 @@
+#include <SFML/Graphics.hpp>
+#include "Gem.h"
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+
+using namespace sf;
+using namespace std;
+
+int main() {
+	cout << "Match 3 Game - Johan Serrano" << endl;
+}
+
